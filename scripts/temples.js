02c82f244b509abcac4b72a0e35540da3b7536c9
MichealOnyemelonu <monyemelonu@gmail.com>
2026-03-14 23:31:51 +0100

    added the temples.js

---
+++ scripts/temples.js
@@ -0,0 +1,25 @@
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+
+document.getElementById("lastModified").textContent =
+  "Last Modified: " + document.lastModified;
+
+
+  
+
+const menuButton = document.getElementById("menu-button");
+const navMenu = document.getElementById("nav-menu");
+
+
+menuButton.addEventListener("click", () => {
+  
+  navMenu.classList.toggle("open");
+
+  
+  if (navMenu.classList.contains("open")) {
+    menuButton.textContent = "✖"; 
+  } else {
+    menuButton.textContent = "☰"; 
+  }
+});
