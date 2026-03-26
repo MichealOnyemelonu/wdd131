095bbb49eb547caf4f1759dda7fe950259b9de78
MichealOnyemelonu <monyemelonu@gmail.com>
2026-03-27 00:36:18 +0100

    corrected the Uncaught TypeError: Cannot set properties of null (setting 'textContent') showing in teh inspect page

--- scripts/place.js
+++ scripts/place.js
@@ -1,37 +1,40 @@
 
-const yearSpan = document.getElementById("year");
-const lastModifiedSpan = document.getElementById("last-modified");
-
+const yearSpan = document.getElementById("currentyear");   
+const lastModifiedSpan = document.getElementById("lastModified"); 
 
 const now = new Date();
-yearSpan.textContent = now.getFullYear();
 
-lastModifiedSpan.textContent = document.lastModified;
 
+if (yearSpan) {
+  yearSpan.textContent = now.getFullYear();
+}
+
+if (lastModifiedSpan) {
+  lastModifiedSpan.textContent = `Last modified: ${document.lastModified}`;
+}
 
 
 const tempElement = document.getElementById("temp");
 const windSpeedElement = document.getElementById("wind-speed");
 const windChillElement = document.getElementById("wind-chill");
 
-const temperature = parseFloat(tempElement.textContent);   
-const windSpeed = parseFloat(windSpeedElement.textContent); 
+if (tempElement && windSpeedElement && windChillElement) {
+  const temperature = parseFloat(tempElement.textContent);
+  const windSpeed = parseFloat(windSpeedElement.textContent);
 
+  function calculateWindChill(t, v) {
+    return (
+      13.12 +
+      0.6215 * t -
+      11.37 * Math.pow(v, 0.16) +
+      0.3965 * t * Math.pow(v, 0.16)
+    );
+  }
 
-function calculateWindChill(t, v) {
-
-  return (
-    13.12 +
-    0.6215 * t -
-    11.37 * Math.pow(v, 0.16) +
-    0.3965 * t * Math.pow(v, 0.16)
-  );
-}
-
-
-if (temperature <= 10 && windSpeed >= 4.8) {
-  const wc = calculateWindChill(temperature, windSpeed);
-  windChillElement.textContent = wc.toFixed(1); 
-} else {
-  windChillElement.textContent = "N/A";
+  if (temperature <= 10 && windSpeed >= 4.8) {
+    const wc = calculateWindChill(temperature, windSpeed);
+    windChillElement.textContent = wc.toFixed(1);
+  } else {
+    windChillElement.textContent = "N/A";
+  }
 }
